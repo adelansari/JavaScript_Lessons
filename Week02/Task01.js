077661767c8da07b7282b544c137efbd332de269
Adel Ansari <adelansari.a@gmail.com>
2024-01-19 10:17:06 +0200

    update folder and add current files

---
+++ Week02/Task01.js
@@ -0,0 +1,6 @@
+// Define a variable called count with an original value of 0 and then increment it on the following line
+
+let count = 0;
+console.log(`Initial value of count is $ `);
+count += 1;
+console.log(`Incremented value of count is $ `);
