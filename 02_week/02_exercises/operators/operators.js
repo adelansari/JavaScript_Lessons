79994e44a4b862d605d1c33a09dd26748d9aefeb
Adel Ansari <adelansari.a@gmail.com>
2024-01-19 13:38:53 +0200

    updated operators

---
+++ 02_week/02_exercises/operators/operators.js
@@ -0,0 +1,23 @@
+/* 
+- Create variables a and b, and assign them integer values.
+- Use add, subtraction, multiply, divide, and modulus operator for a and b. Store each result in separate variables (sum, difference, product, quotient, reminder).
+- The remainder of a divided by b using the modulus operator and store it in a variable called remainder.
+
+- Use console.log() to print the results of each operation to the console.
+
+- Add comments in your code to explain what each operation does.
+*/
+
+const a = 3;
+const b = 8;
+
+const sum = a + b;
+console.log(`${a} + ${b} = ${sum}`);
+const difference = a - b;
+console.log(`${a} - ${b} = ${difference}`);
+const product = a * b;
+console.log(`${a} * ${b} = ${product}`);
+const quotient = a / b;
+console.log(`${a} / ${b} = ${quotient}`);
+const remainder = a % b;
+console.log(`${a} % ${b} = ${remainder}`);
