f062f77fff50915cf1dd620bf5fda4a92b250e64
Adel Ansari <adelansari.a@gmail.com>
2024-01-26 18:47:38 +0200

    some loop exercies

--- 03_week/03_exerices/loops/loops_03.js
+++ 03_week/03_exerices/loops/loops_03.js
@@ -1,1 +1,11 @@
 // Create a program that continuously prompts the user to input distance (in kilometers) and time (in hours) and then calculates the average speed. The program should terminate when the user enters 0 for the distance. Upon receiving a distance of 0, the program should not prompt for any further input.
+
+let distance;
+while (distance != 0) {
+  distance = Number(prompt('Input distance in km:'));
+  if (distance == 0) break;
+
+  let time = Number(prompt('Input time in hours:'));
+  let averageSpeed = distance / time;
+  console.log(averageSpeed);
+}
--- 03_week/03_exerices/loops/loops_04.js
+++ 03_week/03_exerices/loops/loops_04.js
@@ -1,1 +1,14 @@
 // Develop a program that requests the user to input 20 numbers. After all numbers are entered, the program should display how many of these numbers are even.
+
+let numberTotal = 20;
+let evenCount = 0;
+
+while (numberTotal > 0) {
+  let inputNumbers = Number(prompt('Enter any number:'));
+  if (inputNumbers % 2 == 0) {
+    evenCount++;
+  }
+  numberTotal--;
+}
+
+console.log(evenCount);
