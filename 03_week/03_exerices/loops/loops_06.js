cb03821c250561d56379a7f3bd4c84f7612185aa
Adel Ansari <adelansari.a@gmail.com>
2024-01-29 08:09:49 +0200

    finishing the assignment

--- 03_week/03_exerices/loops/loops_06.js
+++ 03_week/03_exerices/loops/loops_06.js
@@ -1,1 +1,36 @@
 // Develop a program that initially asks the user for a single number. Following this, the program should inquire if the user wishes to continue providing numbers with the prompt: 'Do you want to continue giving numbers? (y/n)'. If the user responds with 'y', the program will request another number. If the response is 'n', the program terminates. Upon termination, it calculates and displays the average of all entered numbers.
+
+let giveNumbers = 'y';
+let countNumbers = 0;
+let sumNumbers = 0;
+
+while (giveNumbers == 'y') {
+  let inputNumber = Number(prompt('Please input a single number.'));
+
+  if (isNaN(inputNumber)) {
+    alert('That is not a number. Please try again.');
+    continue;
+  }
+  if (!inputNumber) {
+    alert('The input cannot be empty. Please try again.');
+    continue;
+  }
+  sumNumbers += inputNumber;
+  countNumbers++;
+
+  giveNumbers = String(prompt('Do you want to continue giving numbers? (y/n)'));
+  if (typeof giveNumbers === 'string') {
+    while (giveNumbers != 'n' && giveNumbers != 'y') {
+      giveNumbers = String(
+        prompt('To continue, you must type "y", otherwise type "n".')
+      );
+      if (giveNumbers == 'n') {
+        break;
+      }
+    }
+  }
+}
+
+console.log(
+  `The average of all entered numbers is ${sumNumbers / countNumbers}`
+);
--- 03_week/03_exerices/loops/loops_08.js
+++ 03_week/03_exerices/loops/loops_08.js
@@ -1,1 +1,32 @@
 // Develop a program that initially asks the user how many numbers they wish to input. After receiving this information, the program should then prompt the user to enter each of these numbers. Once all numbers have been entered, the program should determine and display the smallest and biggest number provided by the user.
+
+let numberTotal = Number(prompt('How many numbers would you like to input?'));
+
+while (!Number.isInteger(numberTotal) || numberTotal < 1) {
+  if (!Number.isInteger(numberTotal)) {
+    alert('Please enter an integer number.');
+  }
+  if (numberTotal < 1) {
+    alert('Please enter a positive number');
+  }
+  numberTotal = Number(prompt('How many numbers would you like to input?'));
+}
+
+let min, max;
+
+for (let inputNumber = 1; inputNumber <= numberTotal; inputNumber++) {
+  let userNumbers = Number(prompt(`Enter number ${inputNumber} out of ${numberTotal} :`));
+
+  while (isNaN(userNumbers)) {
+    alert('Please enter a valid number.');
+    userNumbers = Number(prompt(`Enter number ${inputNumber} out of ${numberTotal} :`));
+  }
+
+  if (inputNumber === 1) {
+    min = max = userNumber;
+  } else {
+    userNumber < min ? (min = userNumber) : (max = userNumber);
+  }
+}
+
+console.log(`The largest number was ${max} and the smallest number was ${min}`);
