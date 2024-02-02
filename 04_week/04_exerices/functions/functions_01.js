5ab2060808c1c416fa6c7d31d8a0ee2c201feb11
Adel Ansari <adelansari.a@gmail.com>
2024-02-02 11:30:53 +0200

    function exercise -week 4

--- 04_week/04_exerices/functions/functions_01.js
+++ 04_week/04_exerices/functions/functions_01.js
@@ -2,3 +2,7 @@
 - Write a function that takes two numbers as parameters and returns their sum.
 - Use function keyword and the arrow function
  */
+
+const sum = (num1, num2) => {
+  return console.log(num1 + num2);
+};
