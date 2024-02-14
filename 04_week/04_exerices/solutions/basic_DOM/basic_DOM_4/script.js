bc1f2db02ed8952e722447009441488d61682cd7
Adel Ansari <adelansari.a@gmail.com>
2024-02-14 09:29:34 +0200

    update panckage app with summary

--- 04_week/04_exerices/solutions/basic_DOM/basic_DOM_4/script.js
+++ 04_week/04_exerices/solutions/basic_DOM/basic_DOM_4/script.js
@@ -1,25 +1,76 @@
+// DOM Elements
 const form = document.querySelector('.form-container');
+const customerName = document.querySelector('#customerName');
+const orderButton = document.querySelector('#order-button');
+const typeSelect = document.querySelector('#type');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const totalPriceElement = document.querySelector('#totalPrice');
+const priceBanner = document.querySelector('.price-banner');
 
+// Global Variables
+let total;
+let toppings = [];
+let extras = [];
+
+// Functions
 const pancakePriceCalc = () => {
-  const typeSelect = document.querySelector('#type');
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  const totalPriceElement = document.querySelector('#totalPrice');
-  const priceBanner = document.querySelector('.price-banner');
-
-  let total = parseInt(typeSelect.value);
-
-  for (const item of checkboxes) {
-    if (item.checked) {
-      total += parseInt(item.value);
-    }
-  }
-
+  total = parseInt(typeSelect.value);
+  checkToppings();
   totalPriceElement.textContent = `$${total}`;
 
   priceBanner.animate([{ transform: 'scale(1)' }, { transform: 'scale(1.05)' }, { transform: ' scale(1)' }], {
-    duration: 100,
+    duration: 200,
     iterations: 1,
   });
 };
 
+const addItem = (itemName, category) => {
+  if (category == 'toppings') {
+    toppings.push(itemName);
+  } else {
+    extras.push(itemName);
+  }
+};
+
+const removeItem = () => {};
+
+console.log('toppings array: ', toppings);
+console.log('extras array: ', extras);
+
+const checkToppings = () => {
+  toppings = [];
+  extras = [];
+  for (const item of checkboxes) {
+    const itemName = item.dataset.name;
+    const category = item.dataset.category;
+    if (item.checked) {
+      total += parseInt(item.value);
+      addItem(itemName, category);
+    } else {
+      removeItem(itemName, category);
+    }
+  }
+  console.log('toppings array: ', toppings);
+  console.log('extras array: ', extras);
+};
+
+const displayOrder = () => {
+  const orderType = document.querySelector('#order_type');
+  const orderToppings = document.querySelector('#order_toppings');
+  const orderExtras = document.querySelector('#order_extras');
+  const orderName = document.querySelector('#order_name');
+  const orderPrice = document.querySelector('#order_price');
+
+  let customerNameValue = customerName.value;
+
+  // Displaying the values
+  orderType.textContent = typeSelect.selectedOptions[0].text;
+  orderToppings.textContent = toppings.join(', ');
+  orderExtras.textContent = extras.join(', ');
+  orderName.textContent = customerNameValue;
+  orderPrice.textContent = `€ ${total}`;
+};
+
+// Event Listenors
 form.addEventListener('change', pancakePriceCalc);
+orderButton.addEventListener('click', displayOrder);
