c8cf5583e9a291b46cf9e163bd8f2fb7623f75b1
Adel Ansari <adelansari.a@gmail.com>
2024-03-04 13:02:43 +0200

    some changes with scroll and click events

--- 07_week/07_exercises/portfolio_tricks/main.js
+++ 07_week/07_exercises/portfolio_tricks/main.js
@@ -1,8 +1,15 @@
 const backToTopButton = document.querySelector('#backToTop');
+const header = document.querySelector('header');
 
-const getToTop = () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-};
+backToTopButton.addEventListener('click', () => {
+  document.body.scrollTop = 0; // for Safari
+  document.documentElement.scrollTop = 0; // for Chrome, Firefox and others
+  // header.style.backgroundColor = 'orange';
+  header.classList.toggle('bg');
+});
 
-backToTopButton.addEventListener('click', getToTop);
+window.addEventListener('scroll', () => {
+  document.body.scrollTop > 200 || document.documentElement.scrollTop > 200
+    ? (backToTopButton.style.display = 'block')
+    : (backToTopButton.style.display = 'none');
+});
