9818823aa076f75259e2ffcfc11be1e699a74840
Adel Ansari <adelansari.a@gmail.com>
2024-03-04 14:31:20 +0200

    toggle menu on menuItems click

--- 07_week/07_exercises/portfolio_tricks/main.js
+++ 07_week/07_exercises/portfolio_tricks/main.js
@@ -1,7 +1,13 @@
 const backToTopButton = document.querySelector('#backToTop');
 const header = document.querySelector('header');
-const mobile = document.querySelector('.mobile');
+const mobButton = document.querySelector('.mobile');
 const nav = document.querySelector('nav ul');
+const menuItems = document.querySelectorAll('nav ul li a');
+const btnModal = document.querySelector('.btnModal');
+
+const toggleMenu = () => {
+  nav.classList.toggle('responsive');
+};
 
 backToTopButton.addEventListener('click', () => {
   document.body.scrollTop = 0; // for Safari
@@ -19,6 +25,5 @@ window.addEventListener('scroll', () => {
   }
 });
 
-mobile.addEventListener('click', () => {
-  nav.classList.toggle('responsive');
-});
+mobButton.addEventListener('click', toggleMenu);
+menuItems.forEach((item) => item.addEventListener('click', toggleMenu));
