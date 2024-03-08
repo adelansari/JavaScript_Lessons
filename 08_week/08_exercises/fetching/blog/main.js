89d038ce67da9caa75b8cbef8a5427084ee2cd80
Adel Ansari <adelansari.a@gmail.com>
2024-03-08 15:00:44 +0200

    add users

--- 08_week/08_exercises/fetching/blog/main.js
+++ 08_week/08_exercises/fetching/blog/main.js
@@ -4,22 +4,21 @@
 //     .then((json) => displayData(json));
 // };
 
-const fetchData = async () => {
+// Posts
+const fetchPosts = async () => {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     if (!response.ok) {
       throw new Error('Error status: ${response.status}');
     }
     const data = await response.json();
-    displayData(data);
+    displayPosts(data);
   } catch (error) {
     console.error(error);
   }
 };
 
-fetchData();
-
-const displayData = (data) => {
+const displayPosts = (data) => {
   const container = document.querySelector('#post-container');
 
   data.forEach((post) => {
@@ -31,3 +30,40 @@ const displayData = (data) => {
     container.appendChild(postElement);
   });
 };
+
+// Users
+const fetchUsers = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error('Error status: ${response.status}');
+    }
+    const data = await response.json();
+    displayUsers(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const displayUsers = (data) => {
+  const container = document.querySelector('#user-container');
+
+  data.forEach((user) => {
+    const userElement = document.createElement('div');
+    const { street, suit, city, zipcode, geo } = user.address;
+    const { lat, lng } = geo;
+    userElement.innerHTML = `
+      <p>Name: <span>${user.name}</span></p>
+      <p>Username: <span>${user.username}</span></p>
+      <p>Email: <span>${user.email}</span></p>
+      <p>Address: <span>${street}</span>, <span>${suit}</span>, <span>${city}</span>, <span>${zipcode}</span></p>
+      <p>Geo: Lat <span>${lat}</span> <span>${lng}</span></p>
+      <p>Website: <span>${user.website}</span></p>
+      `;
+    container.appendChild(userElement);
+  });
+};
+
+// fetch and display posts
+// fetchPosts();
+fetchUsers();
